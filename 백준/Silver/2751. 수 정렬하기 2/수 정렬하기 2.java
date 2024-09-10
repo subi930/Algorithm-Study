31e2337dc92d0f97236e5f7ed8f3bd5f1e9edf0b
Subin Choi <125132754+subi930@users.noreply.github.com>
2024-09-10 15:19:08 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 4740 ms, Memory: 188192 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,19 +1,19 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<Integer> list = new ArrayList<>();
+        int[] arr = new int[n];
 
         for(int i = 0; i<n; i++) {
-            int num = Integer.parseInt(br.readLine());
-            list.add(num);
+            arr[i] = Integer.parseInt(br.readLine());
         }
-        Collections.sort(list);
-        for(Integer a : list) System.out.println(a);
+        
+        Arrays.sort(arr);
+        for(int a : arr) System.out.println(a);
     }
 }
